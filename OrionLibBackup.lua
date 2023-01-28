b86404372027bb7bc64f2e18cdca984d60575b3e
vestyx <89847307+vestyx@users.noreply.github.com>
2023-01-28 08:05:55 +0700

    Update OrionLibBackup.lua

--- OrionLibBackup.lua
+++ OrionLibBackup.lua
@@ -471,14 +471,14 @@ function OrionLib:MakeWindow(WindowConfig)
 	local UIHidden = false
 
 	WindowConfig = WindowConfig or {}
-	WindowConfig.Name = WindowConfig.Name or "Prism Hub"
+	WindowConfig.Name = WindowConfig.Name or _G.LoadingName
 	WindowConfig.ConfigFolder = WindowConfig.ConfigFolder or WindowConfig.Name
 	WindowConfig.SaveConfig = WindowConfig.SaveConfig or false
 	WindowConfig.HidePremium = WindowConfig.HidePremium or false
 	if WindowConfig.IntroEnabled == nil then
 		WindowConfig.IntroEnabled = true
 	end
-	WindowConfig.IntroText = WindowConfig.IntroText or "Prism Hub"
+	WindowConfig.IntroText = WindowConfig.IntroText or _G.LoadingName
 	WindowConfig.CloseCallback = WindowConfig.CloseCallback or function() end
 	WindowConfig.ShowIcon = WindowConfig.ShowIcon or false
 	WindowConfig.Icon = WindowConfig.Icon or "rbxassetid://12245574376"
